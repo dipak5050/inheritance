cf354c1125ab791f7e2a384e1a04bbe16005dd36
dipak5050 <dipakbapodra0111@gmail.com>
2023-08-07 09:55:41 +0530

    Add project files.

---
+++ inheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    class a
+    {
+        public void ex()
+        {
+            Console.WriteLine("a");
+        }
+    }
+    class b:a
+    {
+        public void ex()
+        {
+            Console.WriteLine("b");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            a aa=new a();
+            aa.ex();
+            b bb = new b();
+            bb.ex();
+
+            Console.ReadKey();
+        }
+    }
+}
